24d692126f40ec3c181df0ff688f1e297304138c
OsmanBuyuksar <osmanbuyuksar@gmail.com>
2023-09-11 12:23:20 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Map_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.txtBoxLat = new System.Windows.Forms.TextBox();
+            this.txtBoxLon = new System.Windows.Forms.TextBox();
+            this.btnLoadMap = new System.Windows.Forms.Button();
+            this.picBoxHud = new System.Windows.Forms.PictureBox();
+            this.txtBoxRoll = new System.Windows.Forms.TextBox();
+            this.txtBoxPitch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(454, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(671, 594);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.map_MouseClick);
+            // 
+            // txtBoxLat
+            // 
+            this.txtBoxLat.Location = new System.Drawing.Point(348, 422);
+            this.txtBoxLat.Name = "txtBoxLat";
+            this.txtBoxLat.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLat.TabIndex = 1;
+            // 
+            // txtBoxLon
+            // 
+            this.txtBoxLon.Location = new System.Drawing.Point(348, 469);
+            this.txtBoxLon.Name = "txtBoxLon";
+            this.txtBoxLon.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLon.TabIndex = 2;
+            // 
+            // btnLoadMap
+            // 
+            this.btnLoadMap.Location = new System.Drawing.Point(348, 520);
+            this.btnLoadMap.Name = "btnLoadMap";
+            this.btnLoadMap.Size = new System.Drawing.Size(95, 23);
+            this.btnLoadMap.TabIndex = 3;
+            this.btnLoadMap.Text = "Haritayı Yükle";
+            this.btnLoadMap.UseVisualStyleBackColor = true;
+            this.btnLoadMap.Click += new System.EventHandler(this.btnLoadMap_Click);
+            // 
+            // picBoxHud
+            // 
+            this.picBoxHud.Location = new System.Drawing.Point(12, 12);
+            this.picBoxHud.Name = "picBoxHud";
+            this.picBoxHud.Size = new System.Drawing.Size(421, 279);
+            this.picBoxHud.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxHud.TabIndex = 5;
+            this.picBoxHud.TabStop = false;
+            this.picBoxHud.Click += new System.EventHandler(this.picBoxHud_Click);
+            // 
+            // txtBoxRoll
+            // 
+            this.txtBoxRoll.Location = new System.Drawing.Point(151, 452);
+            this.txtBoxRoll.Name = "txtBoxRoll";
+            this.txtBoxRoll.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxRoll.TabIndex = 6;
+            this.txtBoxRoll.TextChanged += new System.EventHandler(this.txtBoxRoll_TextChanged);
+            // 
+            // txtBoxPitch
+            // 
+            this.txtBoxPitch.Location = new System.Drawing.Point(29, 452);
+            this.txtBoxPitch.Name = "txtBoxPitch";
+            this.txtBoxPitch.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPitch.TabIndex = 7;
+            this.txtBoxPitch.TextChanged += new System.EventHandler(this.txtBoxPitch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 520);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 618);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxPitch);
+            this.Controls.Add(this.txtBoxRoll);
+            this.Controls.Add(this.picBoxHud);
+            this.Controls.Add(this.btnLoadMap);
+            this.Controls.Add(this.txtBoxLon);
+            this.Controls.Add(this.txtBoxLat);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.TextBox txtBoxLat;
+        private System.Windows.Forms.TextBox txtBoxLon;
+        private System.Windows.Forms.Button btnLoadMap;
+        private System.Windows.Forms.PictureBox picBoxHud;
+        private System.Windows.Forms.TextBox txtBoxRoll;
+        private System.Windows.Forms.TextBox txtBoxPitch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Map_Project
+{
+    public partial class Form1 : Form
+    {
+        const string apiKey = "**********************************";
+        const float hudCameraViewAngle = 180;
+        const float horizonLength = 50;
+        const int t = 2;
+        float pitch = 0;
+        float roll = 0;
+        Image img = new Bitmap(1, 1);
+        GMapOverlay markers = new GMapOverlay("markers");
+        public Form1()
+        {
+            GMapProviders.GoogleMap.ApiKey = apiKey;
+            InitializeComponent();
+        }
+
+        private void btnLoadMap_Click(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Right;
+            map.MapProvider = GMapProviders.GoogleMap;
+            double lat = Convert.ToDouble(txtBoxLat.Text);
+            double lon = Convert.ToDouble(txtBoxLon.Text);
+            map.MinZoom = 5;
+            map.MaxZoom = 18;
+            map.Zoom = 10;
+            map.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+            map.Position = new GMap.NET.PointLatLng(lat, lon);
+
+            PointLatLng point = new PointLatLng(lat, lon);
+
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.orange_small);
+
+            markers.Markers.Add(marker);
+
+            map.Overlays.Add(markers);
+
+        }
+        private Image Rotate_Image(Image image, float angle)
+        {
+
+            Bitmap img = new Bitmap(image.Width, image.Height);
+
+            Graphics gfx = Graphics.FromImage(img);
+
+            gfx.TranslateTransform(img.Width / 2, img.Height / 2);
+
+            gfx.RotateTransform(angle);
+
+            gfx.DrawImage(image, -img.Width, -img.Height, image.Width*t, image.Height*t);
+
+            gfx.Dispose();
+
+            return img;
+
+        }
+        private Image DrawHUD(PictureBox picBox, float angle)
+        {
+            float h = picBox.Size.Height;
+            float w = picBox.Size.Width;
+            Bitmap img = new Bitmap((int)w, (int)h);
+            Graphics gfx = Graphics.FromImage(img);
+
+
+            if (angle < hudCameraViewAngle / 2 && angle > -hudCameraViewAngle / 2)
+            {
+                float ratio = (hudCameraViewAngle*(t-1)/2 + hudCameraViewAngle / 2 - angle) / (hudCameraViewAngle*(t-1)/ 2 + (hudCameraViewAngle / 2 + angle));//MathF.Tan(hudCameraViewAngle - angle) / MathF.Tan(hudCameraViewAngle + angle);
+                float sky_h = h / (1 / ratio + 1);
+                float grnd_h = h / (ratio + 1);
+
+
+
+                RectangleF sky = new RectangleF(0, 0, w, sky_h);
+                RectangleF grnd = new RectangleF(0, sky_h, w, grnd_h);
+
+                gfx.FillRectangle(Brushes.Aqua, sky);
+                gfx.FillRectangle(Brushes.Brown, grnd);
+            }
+            else if (angle <= -hudCameraViewAngle / 2)
+            {
+                RectangleF sky = new RectangleF(0, 0, w, h);
+                gfx.FillRectangle(Brushes.Aqua, sky);
+            }
+            else if (angle >= hudCameraViewAngle / 2)
+            {
+                RectangleF grnd = new RectangleF(0, 0, w, h);
+                gfx.FillRectangle(Brushes.Brown, grnd);
+            }
+            gfx.DrawImage(img, 0, 0, (int)w, (int)h);
+            gfx.DrawLine(Pens.Red, w/2 -horizonLength/2, h / 2, w/2 + horizonLength/2, h / 2);
+            gfx.DrawString("0", DefaultFont, Brushes.Red, w / 2 + horizonLength / 2, h / 2);
+            gfx.Dispose();
+
+            return img;
+        }
+
+        private void txtBoxRoll_TextChanged(object sender, EventArgs e)
+        {
+            if(float.TryParse(txtBoxRoll.Text, out roll))
+            {
+                roll = (float)Convert.ToDouble(txtBoxRoll.Text);
+                img = DrawHUD(picBoxHud, pitch);
+                picBoxHud.BackgroundImage = Rotate_Image(img, roll);
+            }
+        }
+
+        private void txtBoxPitch_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(txtBoxPitch.Text, out pitch))
+            {
+                pitch = (float)Convert.ToDouble(txtBoxPitch.Text);
+                img = DrawHUD(picBoxHud, pitch);
+                picBoxHud.BackgroundImage = Rotate_Image(img, roll);
+            }
+        }
+
+        private void map_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+                GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.orange_small);
+                markers.Markers.Add(marker);
+
+
+
+
+            }
+        }
+
+        private void picBoxHud_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
